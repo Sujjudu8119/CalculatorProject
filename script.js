1ec0a1ec52435e9aa79742516dce18b307ee58cd
Sujjudu8119 <sujjudu.aws.com>
2026-07-16 17:31:03 +0530

    Created Calculator Project

---
+++ script.js
@@ -0,0 +1,11 @@
+function add(){
+
+let a=parseInt(document.getElementById("num1").value);
+
+let b=parseInt(document.getElementById("num2").value);
+
+let c=a+b;
+
+document.getElementById("result").innerHTML="Result = "+c;
+
+}
